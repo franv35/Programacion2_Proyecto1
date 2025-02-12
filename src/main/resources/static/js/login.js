9b10d3a7ee0a7df61347d18148770d2f9c7823a7
franv35 <you@example.com>
2025-02-12 17:27:35 -0300

    v1.1

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -24,9 +24,10 @@ async function iniciarSesion(){
   });
   
   
-  const respuesta = await request.text();//manera si hay una sesion de usuario
+  const respuesta = await request.text();//maneja si hay una sesion de usuario
 
   if(respuesta=="OK"){
+	//redirige a página deseada
 	window.location.href="usuarios.html";
   }else{
 	alert("Las credenciales no son correctas. Intente nuevamente")
